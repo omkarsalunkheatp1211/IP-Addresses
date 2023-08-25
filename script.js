912ecaa001b8e2afed771d89d8763463cb6d6954
Omkar Salunkhe <96873232+omkarsalunkheatp1211@users.noreply.github.com>
2023-08-25 19:01:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const ipv4AddressElement = document.getElementById("ipv4-address");
 const ipv6AddressElement = document.getElementById("ipv6-address");
+const ipLocationElement = document.getElementById("ip-location");
+const ispElement = document.getElementById("isp");
+const cityElement = document.getElementById("city");
+const timeZoneElement = document.getElementById("time-zone");
 
 const copyIpv4Button = document.getElementById("copy-ipv4");
 const copyIpv6Button = document.getElementById("copy-ipv6");
@@ -30,6 +34,22 @@ fetch("https://ipv6.icanhazip.com")
   })
   .catch(error => {
     ipv6AddressElement.textContent = "Failed to fetch IPv6 address.";
+  });
+
+// Fetch IP location information
+fetch("https://ipapi.co/json/")
+  .then(response => response.json())
+  .then(data => {
+    ipLocationElement.textContent = "My IP Location is: " + data.country_name;
+    ispElement.textContent = "My ISP is: " + data.org;
+    cityElement.textContent = "City: " + data.city;
+    timeZoneElement.textContent = "Time Zone: " + data.timezone;
+  })
+  .catch(error => {
+    ipLocationElement.textContent = "Failed to fetch IP location information.";
+    ispElement.textContent = "Failed to fetch ISP information.";
+    cityElement.textContent = "Failed to fetch city information.";
+    timeZoneElement.textContent = "Failed to fetch time zone information.";
   });
 
 // Function to copy text to clipboard
